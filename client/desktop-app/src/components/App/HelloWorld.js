b8ade273c890cd2ca5f7ebc268065dc32b1e59e6
Mann Mehta <17it051@charusat.edu.in>
2020-09-18 12:20:52 +0530

    Inital File Structure

---
+++ client/desktop-app/src/components/App/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HelloWorld = () => (
+  <div>
+    <h1>Hello, Electron!</h1>
+    <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+  </div>
+);
+
+export default HelloWorld;
