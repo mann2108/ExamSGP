d599c1bf39860648a0e6ae7783ebe08bca6823ef
Mann Mehta <mannmehta2199@gmail.com>
2020-09-19 21:40:09 +0530

    Mongodb Atlas Connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-// Happy Coding
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./DB/connection");
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const Port = process.env.PORT;
+
+app.get("/",(req,res,next) => {
+    res.send("Helloo");
+});
+app.listen(Port, () => console.log("Server Started"));
